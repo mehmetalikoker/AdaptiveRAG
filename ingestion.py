9893a2083fce1b3df0d65908d24eaeeea8c9610b
Mehmet <mehmetalikoker@outlook.com>
2026-03-21 09:30:22 +0300

    first commit

---
+++ ingestion.py
@@ -0,0 +1,7 @@
+from dotenv import load_dotenv
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+
+load_dotenv()
---
+++ main.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+if __name__ == '__main__':
+    print_hi('hi')
+
