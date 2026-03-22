d2ce043f436f025116dc2c850c6fea4bc92c9cc7
Mehmet <mehmetalikoker@outlook.com>
2026-03-22 17:36:03 +0300

    generation node and hallucination_grader added

--- graph/chains/retrieval_grader.py
+++ graph/chains/retrieval_grader.py
@@ -1,7 +1,6 @@
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain_openai import ChatOpenAI
-from ingestion import retriever
 from dotenv import load_dotenv, find_dotenv
 
 
