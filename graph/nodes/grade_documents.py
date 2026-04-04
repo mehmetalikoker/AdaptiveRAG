a1fe400fb8b56f1efc70710185319618fe235a48
Mehmet <mehmetalikoker@outlook.com>
2026-04-04 17:34:03 +0300

    grade_documents code editing

--- graph/nodes/grade_documents.py
+++ graph/nodes/grade_documents.py
@@ -5,18 +5,11 @@ from graph.state import GraphState
 
 
 def grade_documents(state: GraphState) -> Dict[str, Any]:
-    """
-    Determines whether the retrieved documents are relevant to the question
-    If any document is not relevant, we will set a flag to run web search
 
-    Args:
-        state (dict): The current graph state
-
-    Returns:
-        state (dict): Filtered out irrelevant documents and updated web_search state
-    """
-
-    print("---CHECK DOCUMENT RELEVANCE TO QUESTION---")
+    # Determines whether the retrieved documents are relevant to the question
+    # If any document is not relevant, we will set a flag to run web search
+    # state (dict): Filtered out irrelevant documents and updated web_search state
+    print("CHECK DOCUMENT RELEVANCE TO QUESTION")
     question = state["question"]
     documents = state["documents"]
 
@@ -28,10 +21,10 @@ def grade_documents(state: GraphState) -> Dict[str, Any]:
         )
         grade = score.binary_score
         if grade.lower() == "yes":
-            print("---GRADE: DOCUMENT RELEVANT---")
+            print("GRADE: DOCUMENT RELEVANT")
             filtered_docs.append(d)
         else:
-            print("---GRADE: DOCUMENT NOT RELEVANT---")
+            print("GRADE: DOCUMENT NOT RELEVANT")
             web_search = True
             continue
     return {"documents": filtered_docs, "question": question, "web_search": web_search}
