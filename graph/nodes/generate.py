f392da33ef45b1e45e4efa4be27a97c32c37f269
Mehmet <mehmetalikoker@outlook.com>
2026-04-05 18:59:16 +0300

    generate.py updated

--- graph/nodes/generate.py
+++ graph/nodes/generate.py
@@ -5,7 +5,7 @@ from graph.state import GraphState
 
 
 def generate(state: GraphState) -> Dict[str, Any]:
-    print("GENERATE")
+    print("GENERATE STARTING")
     question = state["question"]
     documents = state["documents"]
 
