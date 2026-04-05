8855548355d71523490a45149f910bd826637750
Mehmet <mehmetalikoker@outlook.com>
2026-04-05 15:38:16 +0300

    generate.py change prompt use

--- graph/chains/generation.py
+++ graph/chains/generation.py
@@ -1,12 +1,23 @@
-from langchain import hub
-from langchain_core.output_parsers import StrOutputParser
-from langchain_openai import ChatOpenAI
+import os
 from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
 load_dotenv()
 
+system_prompt = """You are an assistant for question-answering tasks. 
+Use the following pieces of retrieved context to answer the question. 
+If you don't know the answer, just say that you don't know. 
+Use three sentences maximum and keep the answer concise."""
 
-llm = ChatOpenAI(temperature=0)
-prompt = hub.pull("rlm/rag-prompt")
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("human", "Context: {context} \n\n Questions: {question}"),
+    ]
+)
+
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
 
 generation_chain = prompt | llm | StrOutputParser()
