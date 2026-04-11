e36ce05c2e12c8ade37ad1d34ca62479be681398
Mehmet <mehmetalikoker@outlook.com>
2026-04-11 09:53:21 +0300

    test class update for method explanation

--- test/test_adaptive_rag.py
+++ test/test_adaptive_rag.py
@@ -1,26 +1,24 @@
 import os
-import pytest
+
 from dotenv import load_dotenv
 from pathlib import Path
 
-# --- KRİTİK SIRALAMA: Importlardan ÖNCE anahtarı yükle ---
+
 env_path = Path(__file__).parent.parent / ".env"
 load_dotenv(dotenv_path=env_path)
 
-# Eğer hala hata alıyorsan, manuel olarak buraya ata (Sadece test için)
-if not os.getenv("OPENAI_API_KEY"):
-    os.environ["OPENAI_API_KEY"] = "sk-proj-senin-anahtarin"
 
-# --- ŞİMDİ PROJE DOSYALARINI İÇE AKTARABİLİRİZ ---
+
+# --- project file import to into ---
 from graph.graph import app
 from ingestion import retriever
-from langchain_core.messages import AIMessage
+
 
 
 def test_retriever_is_working():
-    """Vektör veritabanından belge dönüp dönmediğini test eder."""
+    """Check if any documents have been returned from the database."""
     question = "RAG nedir?"
-    # LangChain 0.3 standartı: invoke kullanılır
+
     docs = retriever.invoke(question)
 
     assert isinstance(docs, list)
@@ -29,36 +27,35 @@ def test_retriever_is_working():
 
 
 def test_router_decision_logic():
-    """Router'ın doğru düğüme yönlendirme yapıp yapmadığını test eder."""
+    """IMPORTANT : This tests whether the router is routing to the correct node."""
     inputs = {"question": "Fransa'nın başkenti neresidir?"}
 
-    # LangGraph invoke testi
+    # LangGraph invoke tests
     result = app.invoke(inputs)
 
     assert "generation" in result
     assert isinstance(result["generation"], str)
 
 
-import os
-import pytest
+
 from dotenv import load_dotenv
 from pathlib import Path
 
-# 1. .env yükle
+# 1. .env load
 env_path = Path(__file__).parent.parent / ".env"
 load_dotenv(dotenv_path=env_path)
 
 
 def test_generate_node_mocked(mocker):
-    """Generation düğümünü gerçek OpenAI çağrısı yapmadan test eder."""
+    """IMPORTANT : It tests the Generation node without making an actual OpenAI call."""
 
-    # 2. ÖNCE MOCKLA: ChatOpenAI daha oluşmadan biz onun yolunu kesiyoruz
+    # 2. first mock
     mock_llm = mocker.patch("langchain_openai.ChatOpenAI.invoke")
 
     from langchain_core.messages import AIMessage
     mock_llm.return_value = AIMessage(content="Bu bir mock cevaptır.")
 
-    # 3. ŞİMDİ IMPORT ET: Fonksiyonun içinde import ederek erken patlamayı önlüyoruz
+    # 3. import
     from graph.nodes.generate import generate
 
     sample_state = {
@@ -66,8 +63,8 @@ def test_generate_node_mocked(mocker):
         "documents": ["Test dökümanı içeriği"]
     }
 
-    # Çalıştır
+    # run
     result = generate(sample_state)
 
-    # Kontrol et
+    # check result
     assert result["generation"] == "Bu bir mock cevaptır."
